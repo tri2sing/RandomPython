d65aa75f85741c979bdc49dbfe0b8682b11b998b
Sameer Adhikari <tri2sing@yahoo.com>
2016-05-26 22:30:40 -0700

    topological sort

--- leet/topologicalsort.py
+++ leet/topologicalsort.py
@@ -4,17 +4,17 @@ Created on Apr 20, 2016
 @author: Sameer Adhikari
 '''
 
-from collections import defaultdict, deque
+# This works for leetcode problems Course Schedule I & II.
+
+from collections import defaultdict
+
 class Solution(object):
     
     def __init__(self):
         # Data structure to build the graph
-        self.graph = defaultdict(list)
-        self.toposort = deque() 
-        self.num_vertices = 0
-        self.none_mark = -1 # vertex does not have a mark
-        self.temp_mark = -2 # vertex has a temporary mark
-        self.perm_mark = -3 # vertex has a permanent mark
+        self.unvisited = -1 # vertex does not have a mark
+        self.visiting = -2 # vertex has a temporary mark
+        self.visited = -3 # vertex has a permanent mark
     
     def buildGraph(self, num_vertices, edges):
         """
@@ -23,8 +23,10 @@ class Solution(object):
         """
         # Build graph from the input assuming 
         # that the vertex ids are [0, num_vertices-1]
+        self.graph = defaultdict(list)
+        self.toposort = [] # a deque would be better, but leetcode ...
         self.num_vertices = num_vertices
-        self.visited = [self.none_mark for _ in range(num_vertices)]
+        self.visited = [self.unvisited for _ in range(num_vertices)]
         for edge in edges:
             u = edge[0]
             v = edge[1]
@@ -35,21 +37,20 @@ class Solution(object):
         :type node: int
         :rtype: bool:
         """
-        if self.visited[node] == self.temp_mark:
+        if self.visited[node] == self.visiting:
             # Detected a cycle
             return False
-        if self.visited[node] == self.none_mark:
-            self.visited[node] = self.temp_mark
+        if self.visited[node] == self.unvisited:
+            self.visited[node] = self.visiting
             # Got to destination of each edge
             for v in self.graph[node]:
                 result = self.visitNeighbors(v)
                 if not result:
                     return False
-            self.visited[node] = self.perm_mark
-            self.toposort.appendleft(node)
+            self.visited[node] = self.visited
+            self.toposort.append(node)
         return True
             
-        
         
     def canFinish(self, num_vertices, edges):
         """
@@ -59,16 +60,27 @@ class Solution(object):
         """
         self.buildGraph(num_vertices, edges)
         for i in range(num_vertices):
-            if self.visited[i] == self.none_mark:
+            if self.visited[i] == self.unvisited:
                 possible = self.visitNeighbors(i)
                 if not possible:
                     return False
         return True
-            
+     
+    def findOrder(self, num_vertices, edges):
+        if self.canFinish(num_vertices, edges):
+            return self.toposort
+        else:
+            return []       
         
 if __name__ == '__main__':
-    num_vertexes = 2
-    edges = [[0, 1], [1, 0]]
     s = Solution()
-    print s.canFinish(num_vertexes, edges)
+    num_vertices = 2
+    edges = [[0, 1], [1, 0]]
+    print s.findOrder(num_vertices, edges)
+    num_vertices = 2
+    edges = [[1, 0]]
+    print s.findOrder(num_vertices, edges)
+    
+    
+    
     
