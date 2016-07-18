9cd6132493d29f7683a4769d936b7151080e10d6
Sameer Adhikari <tri2sing@yahoo.com>
2016-07-17 17:58:22 -0700

    added recursive min function

--- recursion/nestednumberlist.py
+++ recursion/nestednumberlist.py
@@ -31,3 +31,18 @@ class NestedNumberList(object):
             else:
                 raise TypeError('Wrong type in the input')
         return current_max       
+    
+    def recursive_min(self, nstd_num_list=[]):
+        current_min = float("inf")
+        for item in nstd_num_list:
+            if isinstance(item, numbers.Number):
+                if item < current_min:
+                    current_min = item
+            elif isinstance(item, list):
+                child_min = self.recursive_min(item)
+                if child_min < current_min:
+                    current_min = child_min
+            else:
+                raise TypeError('Wrong type in the input')
+        return current_min       
+        
