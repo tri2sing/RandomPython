82565784ed0d04f9fdff6ba02ec34100ad920965
Sameer Adhikari <tri2sing@yahoo.com>
2016-07-17 18:20:42 -0700

    target occurrence count

--- ztests/test_nestednumberlist.py
+++ ztests/test_nestednumberlist.py
@@ -25,5 +25,21 @@ def test_recursive_max(nstd_num_list):
     assert(nstd_num_list.recursive_max([[[13, 7], 90], 2, [1, 100], 8, 6]) == 100)    
     # Check if a call with incompatible type raises exception
     with pytest.raises(TypeError):
-        nstd_num_list.recursive_sum([2, 9, [1, 13], 'a', 6])
+        nstd_num_list.recursive_max([2, 9, [1, 13], 'a', 6])
     
+def test_recursive_min(nstd_num_list):
+    assert(nstd_num_list.recursive_min([2, 9, [1, 13], 8, 6]) == 1)
+    assert(nstd_num_list.recursive_min([2, [[100, 1], 90], [10, 13], 8, 6]) == 1)
+    assert(nstd_num_list.recursive_min([2, [[13, -7], 90], [1, 100], 8, 6]) == -7)
+    assert(nstd_num_list.recursive_min([[[-13, 7], 90], 2, [1, 100], 8, 6]) == -13)
+    with pytest.raises(TypeError):
+        nstd_num_list.recursive_min([2, 9, [1, 13], 'a', 6])
+            
+def test_recursive_target_count(nstd_num_list):
+    assert(nstd_num_list.recursive_target_count(2, []) == 0)
+    assert(nstd_num_list.recursive_target_count(2, [2, 9, [2, 1, 13, 2], 8, [2, 6]]) == 4)
+    assert(nstd_num_list.recursive_target_count(7, [[9, [7, 1, 13, 2], 8], [7, 6]]) == 2)
+    assert(nstd_num_list.recursive_target_count(15, [[9, [7, 1, 13, 2], 8], [2, 6]]) == 0)
+    assert(nstd_num_list.recursive_target_count(5, [[5, [5, [1, 5], 5], 5], [5, 6]]) == 6)
+    assert(nstd_num_list.recursive_target_count("a",
+         [["this",["a",["thing","a"],"a"],"is"], ["a","easy"]]) == 4)
